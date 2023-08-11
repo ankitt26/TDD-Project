9f3ba5800c54c46bacc27d4be4dff2797bba760b
Ankit <ankit26kushwa@gmail.com>
2023-08-11 12:30:36 +0530

    create method factorial in Solver class

--- classes/solver.rb
+++ classes/solver.rb
@@ -1,2 +1,11 @@
 class Solver
+  def factorial(number)
+    raise 'negative numbers are not allowed' if number.negative?
+
+    answer = 1
+    number.times do |x|
+      answer *= (x + 1)
+    end
+    answer
+  end
 end
