085ced0bc1cb8f317276b6a8c9dd6e417c2ecfa6
Ankit <ankit26kushwa@gmail.com>
2023-08-11 11:51:25 +0530

    write test for factorial method

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -9,4 +9,18 @@ describe Solver do
       expect(@solver).to be_an_instance_of(Solver)
     end
   end
+
+  describe '#factorial' do
+    it 'return 1 for the factorial of 0 ' do
+      expect(@solver.factorial(0)).to eq(1)
+    end
+
+    it 'return 1 for the factorial of 1' do
+      expect(@solver.factorial(1)).to eq(1)
+    end
+
+    it 'raise the exception if argument is negative' do
+      expect(@solver.factorial(-2)).to eq('negative numbers are not allowed')
+    end
+  end
 end
